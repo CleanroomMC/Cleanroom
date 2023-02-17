0d133aeac143f50ac1e8e1ed45ed7c5b0062fac2
Rongmario <rongmario@protonmail.com>
2023-02-17 14:07:09 +0000

    Temp fix EnumHelper reflection hackery

--- src/main/java/net/minecraftforge/common/util/EnumHelper.java
+++ src/main/java/net/minecraftforge/common/util/EnumHelper.java
@@ -57,12 +57,6 @@ import javax.annotation.Nullable;
 
 public class EnumHelper
 {
-    private static Object reflectionFactory      = null;
-    private static Method newConstructorAccessor = null;
-    private static Method newInstance            = null;
-    private static Method newFieldAccessor       = null;
-    private static Method fieldAccessorSet       = null;
-    private static boolean isSetup               = false;
 
     //Some enums are decompiled with extra arguments, so lets check for that
     private static Class<?>[][] commonTypes =
@@ -172,44 +166,6 @@ public class EnumHelper
         return addEnum(HorseArmorType.class, name, textureLocation, armorStrength);
     }
 
-    private static void setup()
-    {
-        if (isSetup)
-        {
-            return;
-        }
-
-        try
-        {
-            Method getReflectionFactory = Class.forName("sun.reflect.ReflectionFactory").getDeclaredMethod("getReflectionFactory");
-            reflectionFactory      = getReflectionFactory.invoke(null);
-            newConstructorAccessor = Class.forName("sun.reflect.ReflectionFactory").getDeclaredMethod("newConstructorAccessor", Constructor.class);
-            newInstance            = Class.forName("sun.reflect.ConstructorAccessor").getDeclaredMethod("newInstance", Object[].class);
-            newFieldAccessor       = Class.forName("sun.reflect.ReflectionFactory").getDeclaredMethod("newFieldAccessor", Field.class, boolean.class);
-            fieldAccessorSet       = Class.forName("sun.reflect.FieldAccessor").getDeclaredMethod("set", Object.class, Object.class);
-        }
-        catch (Exception e)
-        {
-            FMLLog.log.error("Error setting up EnumHelper.", e);
-        }
-
-        isSetup = true;
-    }
-
-    /*
-     * Everything below this is found at the site below, and updated to be able to compile in Eclipse/Java 1.6+
-     * Also modified for use in decompiled code.
-     * Found at: http://niceideas.ch/roller2/badtrash/entry/java_create_enum_instances_dynamically
-     */
-    private static Object getConstructorAccessor(Class<?> enumClass, Class<?>[] additionalParameterTypes) throws Exception
-    {
-        Class<?>[] parameterTypes = new Class[additionalParameterTypes.length + 2];
-        parameterTypes[0] = String.class;
-        parameterTypes[1] = int.class;
-        System.arraycopy(additionalParameterTypes, 0, parameterTypes, 2, additionalParameterTypes.length);
-        return newConstructorAccessor.invoke(reflectionFactory, enumClass.getDeclaredConstructor(parameterTypes));
-    }
-
     private static < T extends Enum<? >> T makeEnum(Class<T> enumClass, @Nullable String value, int ordinal, Class<?>[] additionalTypes, @Nullable Object[] additionalValues) throws Exception
     {
         int additionalParamsCount = additionalValues == null ? 0 : additionalValues.length;
@@ -220,17 +176,33 @@ public class EnumHelper
         {
             System.arraycopy(additionalValues, 0, params, 2, additionalValues.length);
         }
-        return enumClass.cast(newInstance.invoke(getConstructorAccessor(enumClass, additionalTypes), new Object[] {params}));
+        Constructor<?> ctor = enumClass.getDeclaredConstructor(additionalTypes);
+        ctor.setAccessible(true);
+        Object ret = ctor.newInstance(new Object[] {params});
+        return enumClass.cast(ret);
     }
 
     public static void setFailsafeFieldValue(Field field, @Nullable Object target, @Nullable Object value) throws Exception
     {
         field.setAccessible(true);
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-        Object fieldAccessor = newFieldAccessor.invoke(reflectionFactory, field, false);
-        fieldAccessorSet.invoke(fieldAccessor, target, value);
+        try
+        {
+            Method getDeclaredFields0 = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
+            getDeclaredFields0.setAccessible(true);
+            for (Field declaredField : (Field[]) getDeclaredFields0.invoke(Field.class, false))
+            {
+                if ("modifiers".equals(declaredField.getName()))
+                {
+                    declaredField.setAccessible(true);
+                    declaredField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        field.set(target, value);
     }
 
     private static void blankField(Class<?> enumClass, String fieldName) throws Exception
@@ -243,6 +215,20 @@ public class EnumHelper
                 setFailsafeFieldValue(field, enumClass, null);
                 break;
             }
+        }
+        try
+        {
+            Method getDeclaredFields0 = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
+            getDeclaredFields0.setAccessible(true);
+            for (Field declaredField : (Field[]) getDeclaredFields0.invoke(Field.class, false))
+            {
+                declaredField.setAccessible(true);
+                setFailsafeFieldValue(declaredField, enumClass, null);
+                break;
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 
@@ -257,7 +243,6 @@ public class EnumHelper
     @Nullable
     private static <T extends Enum<? >> T addEnum(Class<T> enumType, String enumName, Object... paramValues)
     {
-        setup();
         return addEnum(commonTypes, enumType, enumName, paramValues);
     }
 
@@ -295,11 +280,6 @@ public class EnumHelper
     @Nullable
     private static <T extends Enum<? >> T addEnum(boolean test, final Class<T> enumType, @Nullable String enumName, final Class<?>[] paramTypes, @Nullable Object[] paramValues)
     {
-        if (!isSetup)
-        {
-            setup();
-        }
-
         Field valuesField = null;
         Field[] fields = enumType.getDeclaredFields();
 
@@ -366,7 +346,8 @@ public class EnumHelper
             Exception ex = null;
             try
             {
-                ctr = getConstructorAccessor(enumType, paramTypes);
+                Class<?>[] prefixedParamTypes = { String.class, int.class };
+                ctr = enumType.getDeclaredConstructor(ArrayUtils.addAll(prefixedParamTypes, paramTypes));
             }
             catch (Exception e)
             {
@@ -421,11 +402,4 @@ public class EnumHelper
         }
     }
 
-    static
-    {
-        if (!isSetup)
-        {
-            setup();
-        }
-    }
 }
