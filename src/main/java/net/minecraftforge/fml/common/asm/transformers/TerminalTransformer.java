5e64cc19309d583d983ea8ab10cc5e12552850a8
geeky_kappa <pocketkirby1@gmail.com>
2023-03-25 12:31:11 +0800

    Add back a dummy TerminalTransformer for compatibility

---
+++ src/main/java/net/minecraftforge/fml/common/asm/transformers/TerminalTransformer.java
@@ -0,0 +1,60 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2020.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.fml.common.asm.transformers;
+
+import org.objectweb.asm.*;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+import net.minecraftforge.fml.common.FMLLog;
+
+public class TerminalTransformer implements IClassTransformer
+{
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass)
+    {
+        return basicClass;
+    }
+
+    public static class ExitVisitor extends ClassVisitor
+    {
+        private String clsName = null;
+        private boolean dirty;
+        private static final String callbackOwner = org.objectweb.asm.Type.getInternalName(ExitVisitor.class);
+
+        private ExitVisitor(ClassVisitor cv)
+        {
+            super(Opcodes.ASM9, cv);
+        }
+
+        @Override
+        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces)
+        {
+
+        }
+
+        @Override
+        public MethodVisitor visitMethod(int mAccess, final String mName, final String mDesc, String mSignature, String[] mExceptions)
+        {
+            return new MethodVisitor(Opcodes.ASM9, super.visitMethod(mAccess, mName, mDesc, mSignature, mExceptions)){};
+        }
+
+
+    }
+}
