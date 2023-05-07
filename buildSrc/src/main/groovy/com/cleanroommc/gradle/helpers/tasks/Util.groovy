33db0ef2160bdf3d7d9c0a8f3475c00329bbf109
kappa-maintainer <gkappa@outlands.top>
2023-05-07 22:32:23 +0800

    Gradle 8.1.1 and etc

--- buildSrc/src/main/groovy/com/cleanroommc/gradle/helpers/tasks/Util.groovy
+++ buildSrc/src/main/groovy/com/cleanroommc/gradle/helpers/tasks/Util.groovy
@@ -2,7 +2,6 @@ package com.cleanroommc.gradle.helpers.tasks
 
 import groovy.json.JsonSlurper
 import org.gradle.api.GradleException
-import org.gradle.internal.os.OperatingSystem
 
 import java.security.MessageDigest
 import java.text.SimpleDateFormat
@@ -178,19 +177,21 @@ class Util {
 	}
 
 	static def getCurrentArch() {
-		switch (OperatingSystem.current()) {
-			case OperatingSystem.LINUX:
-				def osArch = System.getProperty("os.arch")
-				return osArch.startsWith("arm") || osArch.startsWith("aarch64")
-						? "natives-linux-${osArch.contains("64") || osArch.startsWith("armv8") ? "arm64" : "arm32"}"
-						: "natives-linux"
-			case OperatingSystem.MAC_OS:
-				return System.getProperty("os.arch").startsWith("aarch64") ? "natives-macos-arm64" : "natives-macos"
-			case OperatingSystem.WINDOWS:
-				def osArch = System.getProperty("os.arch")
-				return osArch.contains("64")
-						? "natives-windows${osArch.startsWith("aarch64") ? "-arm64" : ""}"
-						: "natives-windows-x86"
+		def osName = System.getProperty("os.name").toLowerCase();
+		if (osName.contains("linux")) {
+			def osArch = System.getProperty("os.arch")
+			return osArch.startsWith("arm") || osArch.startsWith("aarch64")
+					? "natives-linux-${osArch.contains("64") || osArch.startsWith("armv8") ? "arm64" : "arm32"}"
+					: "natives-linux"
+		}
+		if (osName.contains("mac os x") || osName.contains("darwin") || osName.contains("osx")) {
+			return System.getProperty("os.arch").startsWith("aarch64") ? "natives-macos-arm64" : "natives-macos"
+		}
+		if (osName.contains("windows")) {
+			def osArch = System.getProperty("os.arch")
+			return osArch.contains("64")
+					? "natives-windows${osArch.startsWith("aarch64") ? "-arm64" : ""}"
+					: "natives-windows-x86"
 		}
 	}
 
