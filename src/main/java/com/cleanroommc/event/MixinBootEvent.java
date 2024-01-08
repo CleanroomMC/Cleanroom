bddc79d6d0c8b80ca3bb0ce7afa02d1f26977815
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-01-09 00:00:36 +0800

    bus

--- src/main/java/com/cleanroommc/event/MixinBootEvent.java
+++ src/main/java/com/cleanroommc/event/MixinBootEvent.java
@@ -4,11 +4,7 @@ import zone.rong.mixinbooter.ILateMixinLoader;
 
 import java.util.List;
 
-/**
- * @Project Cleanroom
- * @Author Hileb
- * @Date 2024/1/8 23:36
- **/
+
 public class MixinBootEvent {
     public record Early(List<Object> mixinLoader) { }
     public record Late(List<ILateMixinLoader> mixinLoader){ }
