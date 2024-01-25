8860cd0c0d6dd634f877458ed48ea431747153da
kappa_maintainer <gkappa@outlands.top>
2024-01-25 11:26:45 +0800

    Add alcCloseDevice to ALC10

--- src/main/java/org/lwjgl/openal/ALC10.java
+++ src/main/java/org/lwjgl/openal/ALC10.java
@@ -53,6 +53,10 @@ public class ALC10 {
         return alcContext;
     }
 
+    public static void alcCloseDevice(ALCdevice alCdevice) {
+        org.lwjgl3.openal.ALC10.alcCloseDevice(alCdevice.device);
+    }
+
     public static ALCcontext alcGetCurrentContext() {
         return alcContext;
     }
