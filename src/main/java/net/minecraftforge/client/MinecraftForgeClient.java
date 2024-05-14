ccc70b50a5a2b626a072f37ddcc023b848ceeefc
strubium <113206902+strubium@users.noreply.github.com>
2024-05-14 12:15:32 -0500

    Requested changes number 1

--- src/main/java/net/minecraftforge/client/MinecraftForgeClient.java
+++ src/main/java/net/minecraftforge/client/MinecraftForgeClient.java
@@ -45,37 +45,19 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-/**
- * This class provides utility methods and functionalities for Minecraft Forge client-side.
- */
+
 public class MinecraftForgeClient
 {
-    /**
-     * Returns the current render pass.
-     *
-     * @return the current render pass
-     */
     public static int getRenderPass()
     {
         return ForgeHooksClient.renderPass;
     }
     
-    /**
-     * Returns the current render layer.
-     *
-     * @return the current render layer
-     */
     public static BlockRenderLayer getRenderLayer()
     {
         return ForgeHooksClient.renderLayer.get();
     }
 
-    /**
-     * Returns the Locale set by the player in Minecraft.
-     * Useful for creating string and number formatters.
-     *
-     * @return the Locale set by the player
-     */
     public static Locale getLocale()
     {
         return Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getJavaLocale();
@@ -87,12 +69,6 @@ public class MinecraftForgeClient
         stencilBits.set(0,8);
     }
 
-    /**
-     * Reserve a stencil bit for use in rendering.
-     * Note: you must check the Framebuffer you are working with to determine if stencil bits are enabled on it before use.
-     *
-     * @return A bit or -1 if no further stencil bits are available
-     */
     public static int reserveStencilBit()
     {
         int bit = stencilBits.nextSetBit(0);
@@ -103,11 +79,6 @@ public class MinecraftForgeClient
         return bit;
     }
 
-    /**
-     * Release the stencil bit for other use
-     *
-     * @param bit The bit from {@link #reserveStencilBit()}
-     */
     public static void releaseStencilBit(int bit)
     {
         if (bit >= 0 && bit < stencilBits.length())
