e08f5e767c793a065ad1ddf1bc1eeee0dc45e471
arcade_kappa <gkappa@outlands.top>
2024-12-24 17:58:02 +0800

    Fix wrong handle

--- src/main/java/com/cleanroommc/client/ime/X11IMEHandler.java
+++ src/main/java/com/cleanroommc/client/ime/X11IMEHandler.java
@@ -1,13 +1,12 @@
 package com.cleanroommc.client.ime;
 
 import org.lwjgl.glfw.GLFW;
-import org.lwjgl.glfw.GLFWNativeX11;
 import org.lwjgl.opengl.Display;
 
 import java.util.function.Consumer;
 
 public class X11IMEHandler implements Consumer<Boolean> {
-    private static final long window = GLFWNativeX11.glfwGetX11Window(Display.getWindow());
+    private static final long window = Display.getWindow();
     @Override
     public void accept(Boolean active) {
         GLFW.glfwSetInputMode(window, GLFW.GLFW_IME, active ? GLFW.GLFW_TRUE : GLFW.GLFW_FALSE);
