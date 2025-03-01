3d70965fc814ff73949f3359a1dda35b47a6a99f
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-03-01 22:30:54 +0800

    remove lwjgl-2 and rewrite transformer

--- src/main/java/net/minecraftforge/fml/common/asm/transformers/LWJGLTransformer.java
+++ src/main/java/net/minecraftforge/fml/common/asm/transformers/LWJGLTransformer.java
@@ -8,65 +8,125 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.commons.ClassRemapper;
 import org.objectweb.asm.commons.Remapper;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 
 public class LWJGLTransformer implements IClassTransformer {
-    private static final LWJGLXRemapper INSTANCE = new LWJGLXRemapper();
-    @Override
-    public byte[] transform(String s, String s1, byte[] bytes) {
-        if (!s1.startsWith("org.lwjgl.")) {
-            return bytes;
-        }
-        String lwjglxName = s.replace("org.lwjgl.", "org.lwjglx.");
-        byte[] lwjglxBytes;
-        try {
-            lwjglxBytes = Launch.classLoader.testGetClassBytes(lwjglxName);
-        } catch (IOException e) {
-            return bytes;
-        }
-        if (lwjglxBytes == null) {
-            return bytes;
-        }
-        ClassReader lwjglxReader = new ClassReader(lwjglxBytes);
-        ClassWriter writer = new ClassWriter(0);
-        ClassVisitor classVisitor = new ClassRemapper(writer, INSTANCE);
-        lwjglxReader.accept(classVisitor, 0);
-        lwjglxBytes = writer.toByteArray();
-        if (bytes == null) {
-            return lwjglxBytes;
-        }
+    private final HashMap<String, MergeTransformer> mergeTransformers = new HashMap<>();
 
-        ClassNode lwjglNode = new ClassNode();
-        ClassReader lwjglReader = new ClassReader(bytes);
-        lwjglReader.accept(lwjglNode, 0);
-
-        lwjglxReader = new ClassReader(lwjglxBytes);
-        ClassNode lwjglxNode = new ClassNode();
-        lwjglxReader.accept(lwjglxNode, 0);
-        Set<String> methods = lwjglNode.methods.stream().map(m -> m.name + m.desc).collect(Collectors.toSet());
-        lwjglxNode.methods.forEach(m -> {
-            if (!methods.contains(m.name + m.desc)) {
-                lwjglNode.methods.add(m);
-            }
-        });
-        Set<String> fields = lwjglNode.fields.stream().map(f -> f.name + f.desc).collect(Collectors.toSet());
-        lwjglxNode.fields.forEach(f -> {
-            if (!fields.contains(f.name + f.desc)) {
-                lwjglNode.fields.add(f);
-            }
-        });
-        if (s1.equals("org.lwjgl.openal.AL")) {
-            lwjglNode.methods.removeIf(m -> m.name.equals("destroy"));
-            lwjglxNode.methods.stream().filter(m -> m.name.equals("destroy")).forEach(m -> lwjglNode.methods.add(m));
-        }
-        ClassWriter out = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-        lwjglNode.accept(out);
-        return out.toByteArray();
+    public LWJGLXTransformer(){
+        mergeTransformers.put("org.lwjgl.openal.AL", (lwjgl, lwjglx, methods, fields) -> lwjglx.methods.stream().filter(m -> m.name.equals("destroy"))
+                .forEach(methodNode -> methods.put(methodNode.name + methodNode.desc, methodNode)));
     }
 
+    @Override
+    public byte[] transform(String unusedString, String name, byte[] bytes) {
+        // TODO : edit the caches in ClassLoader : cachedResources, negativeResourceCache
+        if (!name.startsWith("org.lwjgl.")) {
+            return bytes; // ignored
+        } else {
+            // reload the lwjgl base codes
+            try {
+                bytes = null;
+                var itr = Launch.classLoader.findResources(name.replace('.', '/').concat(".class")).asIterator();
+                while (itr.hasNext()) {
+                    var url = itr.next();
+                    if (!url.getPath().contains("lwjgl-2")) {
+                        try (InputStream stream = url.openStream()) {
+                            bytes = stream.readAllBytes();
+                        }
+                    }
+                }
+                
+            } catch (IOException e) {
+                bytes = null;
+            }
+
+            if (bytes == null) {
+                // Not present
+                String lwjglxName =  "org.lwjglx." + name.substring(10);
+                byte[] lwjglxBytes;
+                try {
+                    lwjglxBytes = Launch.classLoader.testGetClassBytes(lwjglxName);
+                } catch (IOException e) {
+                    return null;
+                }
+                if (lwjglxBytes == null) return null;
+                else {
+                    ClassReader lwjglxReader = new ClassReader(lwjglxBytes);
+                    ClassWriter writer = new ClassWriter(0);
+                    ClassVisitor classVisitor = new ClassRemapper(writer, INSTANCE);
+                    lwjglxReader.accept(classVisitor, 0);
+                    return writer.toByteArray();
+                }
+            } else {
+                // If exists
+                String lwjglxName =  "org.lwjglx." + name.substring(10);
+                byte[] lwjglxBytes;
+                try {
+                    lwjglxBytes = Launch.classLoader.testGetClassBytes(lwjglxName);
+                } catch (IOException e) {
+                    return null;
+                }
+                ClassReader lwjglxReader = new ClassReader(lwjglxBytes);
+                ClassNode lwjglxNode = new ClassNode();
+                lwjglxReader.accept(lwjglxNode, 0);
+
+                ClassNode lwjglNode = new ClassNode();
+                ClassReader lwjglReader = new ClassReader(bytes);
+                lwjglReader.accept(lwjglNode, 0);
+
+                HashMap<String, MethodNode> methods = new HashMap<>();
+                for (MethodNode mn : lwjglxNode.methods) {
+                    methods.put(mn.name + mn.desc, mn);
+                }
+                for (MethodNode mn : lwjglNode.methods) {
+                    methods.put(mn.name + mn.desc, mn);
+                }
+
+                HashMap<String, FieldNode> fields = new HashMap<>();
+                for (FieldNode mn : lwjglxNode.fields) {
+                    fields.put(mn.name + mn.desc, mn);
+                }
+                for (FieldNode mn : lwjglNode.fields) {
+                    fields.put(mn.name + mn.desc, mn);
+                }
+
+                if (mergeTransformers.containsKey(name)) {
+                    mergeTransformers.get(name).runMergeTransform(lwjglNode, lwjglxNode, methods, fields);
+                }
+
+                lwjglNode.fields = List.of(fields.values().toArray(FieldNode[]::new));
+                lwjglNode.methods = List.of(methods.values().toArray(MethodNode[]::new));
+                ClassWriter out = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+                lwjglNode.accept(out);
+                return out.toByteArray();
+            }
+        }
+    }
+
+    public void runMergeTransform(ClassNode lwjgl, ClassNode lwjglx, HashMap<String, MethodNode> methods, HashMap<String, FieldNode> fields) {
+
+    }
+
+    @FunctionalInterface
+    private interface MergeTransformer {
+        void runMergeTransform(ClassNode lwjgl, ClassNode lwjglx, HashMap<String, MethodNode> methods, HashMap<String, FieldNode> fields);
+    }
+
+
+
+    @Override
+    public int getPriority() {
+        return Integer.MAX_VALUE;
+    }
+
+    private static final LWJGLXRemapper INSTANCE = new LWJGLXRemapper();
     static class LWJGLXRemapper extends Remapper {
 
         @Override
@@ -82,5 +142,4 @@ public class LWJGLTransformer implements IClassTransformer {
         }
 
     }
-
 }
