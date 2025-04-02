65dbdd43bd489ba7ac0afb44f4c3be33fa97b306
arcade_kappa <gkappa@outlands.top>
2025-04-02 19:17:32 +0800

    Add config switch for ime blocker

--- src/main/java/com/cleanroommc/client/IMEHandler.java
+++ src/main/java/com/cleanroommc/client/IMEHandler.java
@@ -1,6 +1,7 @@
 package com.cleanroommc.client;
 
 import com.cleanroommc.client.ime.*;
+import net.minecraftforge.common.ForgeModContainer;
 import net.minecraftforge.fml.common.FMLLog;
 import org.lwjgl.glfw.GLFW;
 
@@ -9,14 +10,18 @@ import java.util.function.Consumer;
 public class IMEHandler {
     private static final Consumer<Boolean> instance;
     static {
-        switch (GLFW.glfwGetPlatform()) {
-            case GLFW.GLFW_PLATFORM_WIN32 -> instance = new WindowsIMEHandler();
-            case GLFW.GLFW_PLATFORM_X11 -> instance = new X11IMEHandler();
-            case GLFW.GLFW_PLATFORM_WAYLAND -> instance = new WaylandIMEhandler();
-            default -> {
-                instance = new DummyIMEHandler();
-                FMLLog.log.warn("IME handler initialization failed: Unsupported platform {}", GLFW.glfwGetPlatform());
+        if (ForgeModContainer.inputMethodBlockingEnabled) {
+            switch (GLFW.glfwGetPlatform()) {
+                case GLFW.GLFW_PLATFORM_WIN32 -> instance = new WindowsIMEHandler();
+                case GLFW.GLFW_PLATFORM_X11 -> instance = new X11IMEHandler();
+                case GLFW.GLFW_PLATFORM_WAYLAND -> instance = new WaylandIMEhandler();
+                default -> {
+                    instance = new DummyIMEHandler();
+                    FMLLog.log.warn("IME handler initialization failed: Unsupported platform {}", GLFW.glfwGetPlatform());
+                }
             }
+        } else {
+            instance = new DummyIMEHandler();
         }
     }
     public static void setIME(boolean active) {
