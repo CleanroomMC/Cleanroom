8e4f6af93cf7f103504ef7398d6efde72495c720
ZZZank <3410764033@qq.com>
2025-05-13 19:50:08 +0800

    add test, change some comments

--- src/test/java/net/minecraftforge/fml/common/eventhandler/EventBusTest.java
+++ src/test/java/net/minecraftforge/fml/common/eventhandler/EventBusTest.java
@@ -1,5 +1,6 @@
 package net.minecraftforge.fml.common.eventhandler;
 
+import net.minecraftforge.fml.common.eventhandler.impl.AbnormalListeners;
 import net.minecraftforge.fml.common.eventhandler.impl.ExampleEvent;
 import net.minecraftforge.fml.common.eventhandler.impl.InstanceListeners;
 import net.minecraftforge.fml.common.eventhandler.impl.StaticListeners;
@@ -44,5 +45,17 @@ public class EventBusTest {
             List.of(EventPriority.HIGHEST, EventPriority.NORMAL, EventPriority.LOWEST),
             StaticListeners.triggered
         );
+    }
+
+    @Test
+    public void registerAbnormal() {
+        var bus = new EventBus();
+
+        bus.register(AbnormalListeners.Actual.class);
+
+        var event = new ExampleEvent();
+        bus.post(event);
+
+        Assertions.assertTrue(AbnormalListeners.nonVoid, "listener with non-void return type is valid");
     }
 }
