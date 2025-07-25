8bb102f910ecdf4e655ac622313a1df40a5969f9
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2025-07-25 16:19:35 +0800

    Add Kirino

---
+++ src/main/java/com/cleanroommc/kirino/Kirino.java
@@ -0,0 +1,9 @@
+package com.cleanroommc.kirino;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Kirino
+{
+    public static final Logger LOGGER = LogManager.getLogger("Kirino Rendering");
+}
