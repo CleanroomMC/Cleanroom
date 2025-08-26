595b361638cd815e9dca1cbc5519590eadefce2b
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-08-26 20:46:47 +0800

    Update CatalogueContainer.java

--- src/main/java/com/cleanroommc/common/CatalogueContainer.java
+++ src/main/java/com/cleanroommc/common/CatalogueContainer.java
@@ -16,10 +16,15 @@ public class CatalogueContainer extends DummyModContainer {
         meta.description = "Updates Forge's mod list with a new and improved design";
         meta.version = "1.0.0";
         meta.authorList = Arrays.asList("MrCrayfish", "RuiXuqi");
+        meta.url = "https://github.com/RuiXuqi/Catalogue-Vintage";
+        meta.issueTrackerUrl = "https://github.com/RuiXuqi/Catalogue-Vintage/issues";
+        meta.credits = "Hatsondogs for creating icons";
+        meta.license = "MIT";
         meta.logoFile = "/catalogue_icon.png";
         meta.logoBlur = false;
         meta.iconFile = "/catalogue_icon.png";
         meta.iconBlur = false;
+        meta.iconItem = "minecraft:diamond_sword";
         meta.backgroundFile = "/catalogue_background.png";
     }
 
