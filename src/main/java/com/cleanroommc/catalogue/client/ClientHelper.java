3b55025793e87366caddedbaa823efe1fc3a589e
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-08-29 20:37:35 +0800

    Update ClientHelper.java

--- src/main/java/com/cleanroommc/catalogue/client/ClientHelper.java
+++ src/main/java/com/cleanroommc/catalogue/client/ClientHelper.java
@@ -1,7 +1,6 @@
 package com.cleanroommc.catalogue.client;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import org.lwjgl.opengl.GL11;
 
