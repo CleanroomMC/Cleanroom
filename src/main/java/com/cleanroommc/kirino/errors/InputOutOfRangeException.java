814be295bff35b9611eff50942e081ab642d76ac
Kuba663 <kuba307@poczta.onet.pl>
2025-10-08 20:44:30 +0200

    FSM InputOutOfRangeException

---
+++ src/main/java/com/cleanroommc/kirino/errors/InputOutOfRangeException.java
@@ -0,0 +1,7 @@
+package com.cleanroommc.kirino.errors;
+
+public final class InputOutOfRangeException extends RuntimeException {
+    public InputOutOfRangeException(String message) {
+        super(message);
+    }
+}
