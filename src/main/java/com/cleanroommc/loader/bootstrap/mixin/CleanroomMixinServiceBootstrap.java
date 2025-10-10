df1e1f983b239095619a1986b1ea10866708da72
Rongmario <rongmario@proton.me>
2025-10-10 11:25:05 +0100

    Bootstrap

---
+++ src/main/java/com/cleanroommc/loader/bootstrap/mixin/CleanroomMixinServiceBootstrap.java
@@ -0,0 +1,21 @@
+package com.cleanroommc.loader.bootstrap.mixin;
+
+import org.spongepowered.asm.service.IMixinServiceBootstrap;
+
+public class CleanroomMixinServiceBootstrap implements IMixinServiceBootstrap {
+
+    @Override
+    public String getName() {
+        return "MixinBooter";
+    }
+
+    @Override
+    public String getServiceClassName() {
+        return this.getClass().getPackageName() + ".CleanroomMixinService";
+    }
+
+    @Override
+    public void bootstrap() { }
+
+}
+
