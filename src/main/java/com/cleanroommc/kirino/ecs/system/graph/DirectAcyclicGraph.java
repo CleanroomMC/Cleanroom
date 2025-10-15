cddd4643cd0274f3c49c9334c3bec472932eea13
d3f0k0 <hellomeandyou09@gmail.com>
2025-10-15 12:57:59 +0700

    Starting on Direct Acyclic Graph

---
+++ src/main/java/com/cleanroommc/kirino/ecs/system/graph/DirectAcyclicGraph.java
@@ -0,0 +1,35 @@
+package com.cleanroommc.kirino.ecs.system.graph;
+
+import com.google.common.graph.MutableNetwork;
+import com.google.common.graph.NetworkBuilder;
+import com.google.common.graph.Traverser;
+
+// I know, its beta after all
+@SuppressWarnings("UnstableApiUsage")
+public class DirectAcyclicGraph {
+    private final MutableNetwork<Node, Edge> graph;
+
+    public DirectAcyclicGraph() {
+        this.graph = NetworkBuilder.directed()
+                .allowsSelfLoops(false)
+                .allowsParallelEdges(true)
+                .build();
+    }
+    public void addEdge(Node from, Node to, Edge edge) {
+        Traverser.forGraph(graph.asGraph()).breadthFirst(to).forEach((node) -> {
+            if (node == from) {
+                throw new IllegalArgumentException("Connecting node" + from + "to" + to + "create a cycle!");
+            }
+        });
+        graph.addEdge(from, to, edge);
+    }
+
+    public void addNode(Node node) {
+        graph.addNode(node);
+    }
+
+    public void execute() {
+
+    }
+
+}
