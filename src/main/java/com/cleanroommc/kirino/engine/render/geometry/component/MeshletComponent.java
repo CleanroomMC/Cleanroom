746d81550730a0f0e8cd580e856dd3fd01926e8e
Kuba663 <kuba307@poczta.onet.pl>
2025-10-16 18:29:23 +0200

    Adding fields to the MeshletComponent
    Apparently arrays are not allowed in components so I had to make a variable for each block

--- src/main/java/com/cleanroommc/kirino/engine/render/geometry/component/MeshletComponent.java
+++ src/main/java/com/cleanroommc/kirino/engine/render/geometry/component/MeshletComponent.java
@@ -3,8 +3,43 @@ package com.cleanroommc.kirino.engine.render.geometry.component;
 import com.cleanroommc.kirino.ecs.component.ICleanComponent;
 import com.cleanroommc.kirino.ecs.component.scan.CleanComponent;
 import com.cleanroommc.kirino.engine.render.geometry.AABB;
+import com.cleanroommc.kirino.engine.render.geometry.Block;
+import org.joml.Vector3f;
 
 @CleanComponent
 public class MeshletComponent implements ICleanComponent {
     public AABB aabb;
+    public int normal; // just use the face flags (like in block) for easier calculations down the line
+    public Block block1;
+    public Block block2;
+    public Block block3;
+    public Block block4;
+    public Block block5;
+    public Block block6;
+    public Block block7;
+    public Block block8;
+    public Block block9;
+    public Block block10;
+    public Block block11;
+    public Block block12;
+    public Block block13;
+    public Block block14;
+    public Block block15;
+    public Block block16;
+    public Block block17;
+    public Block block18;
+    public Block block19;
+    public Block block20;
+    public Block block21;
+    public Block block22;
+    public Block block23;
+    public Block block24;
+    public Block block25;
+    public Block block26;
+    public Block block27;
+    public Block block28;
+    public Block block29;
+    public Block block30;
+    public Block block31;
+    public Block block32;
 }
