50fe384c7cee4c63666e73110226737c523cd07a
Kuba663 <kuba307@poczta.onet.pl>
2025-10-16 10:32:30 +0200

    Documentation on compress

--- src/main/java/com/cleanroommc/kirino/engine/render/geometry/Block.java
+++ src/main/java/com/cleanroommc/kirino/engine/render/geometry/Block.java
@@ -24,6 +24,76 @@ public class Block {
         this.faces = faces;
     }
 
+    /**
+     * <p>Compresses the block rendering record into an integer</p>
+     * <p>
+     *     The integer is formatted as: <table>
+     *         <tr>
+     *             <th>17</th>
+     *             <th>16</th>
+     *             <th>15</th>
+     *             <th>14</th>
+     *             <th>13</th>
+     *             <th>12</th>
+     *             <th>11</th>
+     *             <th>10</th>
+     *             <th>9</th>
+     *             <th>8</th>
+     *             <th>7</th>
+     *             <th>6</th>
+     *             <th>5</th>
+     *             <th>4</th>
+     *             <th>3</th>
+     *             <th>2</th>
+     *             <th>1</th>
+     *             <th>0</th>
+     *         </tr>
+     *         <tr>
+     *             <th>z</th>
+     *             <th>z</th>
+     *             <th>z</th>
+     *             <th>z</th>
+     *             <th>y</th>
+     *             <th>y</th>
+     *             <th>y</th>
+     *             <th>y</th>
+     *             <th>x</th>
+     *             <th>x</th>
+     *             <th>x</th>
+     *             <th>x</th>
+     *             <th>f</th>
+     *             <th>f</th>
+     *             <th>f</th>
+     *             <th>f</th>
+     *             <th>f</th>
+     *             <th>f</th>
+     *         </tr>
+     *     </table>
+     * </p>
+     * <p>
+     *     Where:
+     *     <table>
+     *         <tr>
+     *             <th>x</th>
+     *             <th>the x position of the block in the 16x16x16 cutout of the chunk</th>
+     *         </tr>
+     *         <tr>
+     *             <th>y</th>
+     *             <th>the y position of the block in the 16x16x16 cutout of the chunk</th>
+     *         </tr>
+     *         <tr>
+     *             <th>z</th>
+     *             <th>the z position of the block in the 16x16x16 cutout of the chunk</th>
+     *         </tr>
+     *         <tr>
+     *             <th>f</th>
+     *             <th>A bitfield of the sides to be rendered. From the right in order:
+     *             Down, Up, North, South, West, East</th>
+     *         </tr>
+     *     </table>
+     * </p>
+     * @return The integer as described above
+     */
     int compress() {
         return (position.x & 0b1111) << 14
                 | (position.y & 0b1111) << 10
