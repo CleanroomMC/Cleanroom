dee6f19076472c2150443a3433e9d89127976657
Kuba663 <kuba307@poczta.onet.pl>
2025-11-08 12:08:52 +0100

    kD-Tree construction test.
    Oriented around the construction not throwing an error when it's ran.

--- src/test/java/com/cleanroommc/test/kirino/adt/KDTreeTest.java
+++ src/test/java/com/cleanroommc/test/kirino/adt/KDTreeTest.java
@@ -7,7 +7,9 @@ import org.joml.Vector3f;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class KDTreeTest {
     @Test
@@ -33,5 +35,26 @@ public class KDTreeTest {
         var neighbours = kdTree.knn(new Vector3f(14,14,14), 32, 1);
         assertTrue(neighbours.isPresent());
         assertTrue(neighbours.get().contains(near));
+    }
+
+    @Test
+    public void testConstruction() {
+        Random rng = new Random();
+        rng.setSeed(114514);
+
+        List<Meshlet> list = new ArrayList<>();
+        for (int x = 0; x < 16; x++) {
+            for (int y = 0; y < 16; y++) {
+                for (int z = 0; z < 16; z++) {
+                    if (rng.nextBoolean()) {
+                        list.add(new Meshlet(EnumFacing.DOWN, x, y, z, false));
+                    }
+                }
+            }
+        }
+
+        KDTree kdTree = new KDTree();
+
+        assertDoesNotThrow(() -> kdTree.add(list));
     }
 }
