366014821f43941c86fc362d352a7a01f25c5146
jchung01 <jjchung276@gmail.com>
2025-11-14 15:29:51 -0800

    Fix meshlet gen test

--- src/test/java/com/cleanroommc/test/kirino/meshlet/MeshletGenerationTest.java
+++ src/test/java/com/cleanroommc/test/kirino/meshlet/MeshletGenerationTest.java
@@ -105,6 +105,6 @@ public class MeshletGenerationTest {
         }
         System.out.println(generatedMeshlets);
         System.out.println(generatedMeshlets.size());
-        assertTrue("You fucking idiot!", generatedMeshlets.size() < 8);
+        assertTrue("You fucking idiot!", generatedMeshlets.size() == 8);
     }
 }
