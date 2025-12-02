7fcb45cd4d833ca3c3c809b74dff3e51d50b01df
arcade_kappa <gkappa@outlands.top>
2025-12-02 19:26:38 +0800

    Use custom FG6 so we can update to Gradle 9

--- buildSrc/src/main/groovy/com/cleanroommc/gradle/helpers/tasks/Util.groovy
+++ buildSrc/src/main/groovy/com/cleanroommc/gradle/helpers/tasks/Util.groovy
@@ -2,6 +2,7 @@ package com.cleanroommc.gradle.helpers.tasks
 
 import groovy.json.JsonSlurper
 import org.gradle.api.GradleException
+import org.apache.commons.codec.digest.DigestUtils
 
 import java.security.MessageDigest
 import java.text.SimpleDateFormat
@@ -197,9 +198,9 @@ class Util {
 		new Date().iso8601()
 	}
 
-	static def sha1(file) {
+	static def sha1(File file) {
 		MessageDigest md = MessageDigest.getInstance('SHA-1')
-		file.eachByte 4096, { bytes, size ->
+		file.eachByte 4096, {byte[] bytes, int size ->
 			md.update(bytes, 0, size)
 		}
 		return md.digest().collect {String.format "%02x", it}.join()
