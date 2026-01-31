45cde05590dbc63b3efbe068653528dee7af9612
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-01-31 16:55:40 +0800

    Format

--- src/main/java/com/cleanroommc/catalogue/client/screen/CatalogueModListScreen.java
+++ src/main/java/com/cleanroommc/catalogue/client/screen/CatalogueModListScreen.java
@@ -788,10 +788,9 @@ public class CatalogueModListScreen extends GuiScreen implements DropdownMenuHan
         private String getFormattedModName(boolean favouriteIconVisible) {
             String name = this.data.getDisplayName();
             name = this.getFormattedText(name, favouriteIconVisible);
-            return data.getType().getStyle() + name;
+            return this.data.getType().getStyle() + name;
         }
 
-        @NotNull
         private String getFormattedModVersion(boolean favouriteIconVisible) {
             String version = this.data.getVersion();
             if (version.isBlank()) version = this.data.getInnerVersion();
