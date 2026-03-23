21c66cda45fab8bd0d65f43a8f15a0d31125ba48
arcade_kappa <gkappa@outlands.top>
2026-03-23 13:02:31 +0800

    Merge two icu4j instance holders

--- src/main/java/com/cleanroommc/client/ICU4JInstances.java
+++ src/main/java/com/cleanroommc/client/ICU4JInstances.java
@@ -2,11 +2,13 @@ package com.cleanroommc.client;
 
 import com.ibm.icu.segmenter.LocalizedSegmenter;
 import com.ibm.icu.segmenter.Segmenter;
+import com.ibm.icu.text.BreakIterator;
 import com.ibm.icu.util.ULocale;
 
-public class SegmenterHolder {
-    private SegmenterHolder() {}
+public class ICU4JInstances {
+    private ICU4JInstances() {}
 
+    public static final BreakIterator BREAK_ITERATOR = BreakIterator.getLineInstance();
     public static final Segmenter SEGMENTER = LocalizedSegmenter.builder()
             .setLocale(ULocale.getDefault())
             .setSegmentationType(LocalizedSegmenter.SegmentationType.LINE)
