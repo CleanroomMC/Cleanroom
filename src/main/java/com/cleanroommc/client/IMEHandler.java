431b86c4896c5f8b7817471f4717be7089b60171
arcade_kappa <gkappa@outlands.top>
2026-04-22 22:55:13 +0800

    Add SDL, replace GLFW with SDL in IME handling

--- src/main/java/com/cleanroommc/client/IMEHandler.java
+++ src/main/java/com/cleanroommc/client/IMEHandler.java
@@ -1,30 +1,14 @@
 package com.cleanroommc.client;
 
-import com.cleanroommc.client.ime.*;
-import net.minecraftforge.common.ForgeModContainer;
-import net.minecraftforge.fml.common.FMLLog;
-import org.lwjgl.glfw.GLFW;
-
-import java.util.function.Consumer;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.sdl.SDLKeyboard;
 
 public class IMEHandler {
-    private static final Consumer<Boolean> instance;
-    static {
-        if (ForgeModContainer.inputMethodBlockingEnabled) {
-            switch (GLFW.glfwGetPlatform()) {
-                case GLFW.GLFW_PLATFORM_WIN32 -> instance = new WindowsIMEHandler();
-                case GLFW.GLFW_PLATFORM_X11 -> instance = new X11IMEHandler();
-                case GLFW.GLFW_PLATFORM_WAYLAND -> instance = new WaylandIMEhandler();
-                default -> {
-                    instance = new DummyIMEHandler();
-                    FMLLog.log.warn("IME handler initialization failed: Unsupported platform {}", GLFW.glfwGetPlatform());
-                }
-            }
-        } else {
-            instance = new DummyIMEHandler();
-        }
-    }
     public static void setIME(boolean active) {
-        instance.accept(active);
+        if (active) {
+            SDLKeyboard.SDL_StartTextInput(Display.getWindow());
+        } else {
+            SDLKeyboard.SDL_StopTextInput(Display.getWindow());
+        }
     }
 }
