7c11bf0188c3eadfe9e28c844a4a21f8b884df5f
arcade_kappa <gkappa@outlands.top>
2026-04-26 10:23:09 +0800

    Make Lwjgl3Aware annotation for package

--- src/main/java/com/cleanroommc/cleanroom/client/Lwjgl3Aware.java
+++ src/main/java/com/cleanroommc/cleanroom/client/Lwjgl3Aware.java
@@ -5,6 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE, ElementType.PACKAGE})
+@Target({ElementType.PACKAGE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Lwjgl3Aware {}
--- src/main/java/net/minecraftforge/fml/common/asm/transformers/LWJGLTransformer.java
+++ src/main/java/net/minecraftforge/fml/common/asm/transformers/LWJGLTransformer.java
@@ -2,7 +2,6 @@ package net.minecraftforge.fml.common.asm.transformers;
 
 import com.cleanroommc.cleanroom.client.Lwjgl3Aware;
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
@@ -18,44 +17,19 @@ public class LWJGLTransformer implements IClassTransformer {
 
     @Override
     public byte[] transform(String s, String s1, byte[] bytes, Package pkg, Manifest manifest) {
-        if (s1.startsWith("net.minecraft.")
-                || s1.startsWith("com.cleanroommc.cleanroom.")
-                || s1.startsWith("com.cleanroommc.kirino.")) return bytes;
+        if (s1.startsWith("net.minecraft.")) return bytes;
         if (pkg.isAnnotationPresent(Lwjgl3Aware.class)) return bytes;
         var attributes = manifest.getMainAttributes();
         if (attributes.containsKey("Lwjgl3-Aware")
                 && attributes.getValue("Lwjgl3-Aware").equals("true")) return bytes;
         ClassReader reader = new ClassReader(bytes);
         ClassWriter writer = new ClassWriter(0);
-        ClassVisitor cv = new LWJGLXClassRemapper(writer, INSTANCE);
+        ClassVisitor cv = new ClassRemapper(writer, INSTANCE);
         reader.accept(cv, 0);
         return writer.toByteArray();
     }
 
-    private static class LWJGLXClassRemapper extends ClassRemapper {
-
-        public LWJGLXClassRemapper(ClassVisitor classVisitor, Remapper remapper) {
-            super(Opcodes.ASM9, classVisitor, remapper);
-        }
-
-        @Override
-        public AnnotationVisitor visitAnnotation(final String descriptor, final boolean visible) {
-            if (descriptor.equals("Lcom/cleanroommc/cleanroom/client/Lwjgl3Aware;")) {
-                ((LWJGLXRemapper) remapper).setBypass(true);
-            }
-            return super.visitAnnotation(descriptor, visible);
-        }
-
-        @Override
-        public void visitEnd() {
-            ((LWJGLXRemapper) remapper).setBypass(false);
-            super.visitEnd();
-        }
-    }
-
     private static class LWJGLXRemapper extends Remapper {
-
-        private boolean bypass = false;
 
         public LWJGLXRemapper() {
             super(Opcodes.ASM9);
@@ -67,15 +41,11 @@ public class LWJGLTransformer implements IClassTransformer {
                 return null;
             }
 
-            if (!bypass && typeName.startsWith("org/lwjgl/")) {
+            if (typeName.startsWith("org/lwjgl/")) {
                 return "org/lwjglx/" + typeName.substring(10);
             }
 
             return typeName;
-        }
-
-        public void setBypass(boolean newStatus) {
-            bypass = newStatus;
         }
     }
 }
