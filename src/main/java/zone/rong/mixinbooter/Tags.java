6867c767fd06e73148a3b3d00011333c7713b161
Rongmario <rongmario@proton.me>
2026-07-02 17:30:45 +0100

    MB 11.1, read meta from mixinbooter.info + logo

--- src/main/java/zone/rong/mixinbooter/Tags.java
+++ src/main/java/zone/rong/mixinbooter/Tags.java
@@ -1,12 +1,10 @@
 package zone.rong.mixinbooter;
 
-import net.minecraftforge.common.ForgeEarlyConfig;
-
 public final class Tags {
 
     public static final String MOD_ID = "mixinbooter";
     public static final String MOD_NAME = "MixinBooter";
-    public static final String VERSION = ForgeEarlyConfig.CUSTOM_BUILT_IN_MOD_VERSION ? ForgeEarlyConfig.MIXIN_BOOTER_VERSION : "11.0";
+    public static final String VERSION = "11.1";
 
     private Tags() { }
 
