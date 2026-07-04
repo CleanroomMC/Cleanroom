4352bd95b2de2996e4f589357fee24b6c92cb471
Rongmario <rongmario@proton.me>
2026-07-04 01:52:38 +0100

    Make IdentifiedMods' list mutable for Loader

--- src/main/java/com/cleanroommc/discovery/IdentifiedMods.java
+++ src/main/java/com/cleanroommc/discovery/IdentifiedMods.java
@@ -3,12 +3,13 @@ package com.cleanroommc.discovery;
 import net.minecraftforge.fml.common.ModContainer;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public record IdentifiedMods(List<ModContainer> mods, List<File> nonModLibs) {
 
     public IdentifiedMods {
-        mods = List.copyOf(mods);
+        mods = new ArrayList<>(mods); // Can't be unmodifiable
         nonModLibs = List.copyOf(nonModLibs);
     }
 
