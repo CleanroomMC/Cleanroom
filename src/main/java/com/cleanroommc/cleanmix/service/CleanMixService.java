4426b960618d9b2e32b027fa7723b356e17d5cd2
Rongmario <rongmario@proton.me>
2026-07-06 02:24:28 +0100

    Submit mixin containers

--- src/main/java/com/cleanroommc/cleanmix/service/CleanMixService.java
+++ src/main/java/com/cleanroommc/cleanmix/service/CleanMixService.java
@@ -1,8 +1,11 @@
 package com.cleanroommc.cleanmix.service;
 
+import com.cleanroommc.cleanmix.CleanMixModContainer;
 import com.cleanroommc.discovery.CleanroomModDiscoverer;
 import com.google.common.base.Strings;
 import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
+import org.spongepowered.asm.launch.platform.container.ContainerHandleURI;
+import org.spongepowered.asm.launch.platform.container.IContainerHandle;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.extensibility.IRemapper;
@@ -13,6 +16,9 @@ import org.spongepowered.asm.service.mojang.AbstractMixinServiceLaunchWrapper;
 
 import java.io.File;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class CleanMixService extends AbstractMixinServiceLaunchWrapper {
 
@@ -45,6 +51,20 @@ public class CleanMixService extends AbstractMixinServiceLaunchWrapper {
                 Mixins.addConfiguration(singleMixinConfig.trim());
             }
         }
+    }
+
+    @Override
+    public IContainerHandle getPrimaryContainer() {
+        return new ContainerHandleURI(CleanMixModContainer.location().toURI());
+    }
+
+    @Override
+    public Collection<IContainerHandle> getMixinContainers() {
+        List<IContainerHandle> containers = new ArrayList<>();
+        for (File jar : CleanroomModDiscoverer.instance().manifestMixinJars()) {
+            containers.add(new ContainerHandleURI(jar.toURI()));
+        }
+        return containers;
     }
 
     @Override
