b7c14f9bb1095df99562f5d03252048ec0272bc3
Kuba663 <kuba307@poczta.onet.pl>
2026-07-09 18:44:03 +0200

    Context creation

--- src/main/java/com/cleanroommc/cleanroom/compute/ComputeSetup.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/ComputeSetup.java
@@ -1,5 +1,7 @@
 package com.cleanroommc.cleanroom.compute;
 
+import com.cleanroommc.cleanroom.compute.errors.InvalidPlatformError;
+import com.cleanroommc.cleanroom.compute.errors.UnavaliableDeviceError;
 import it.unimi.dsi.fastutil.PriorityQueue;
 import it.unimi.dsi.fastutil.objects.ObjectHeapPriorityQueue;
 import org.apache.logging.log4j.Logger;
@@ -7,6 +9,7 @@ import org.jspecify.annotations.NonNull;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.opencl.CL;
 import org.lwjgl.opencl.CL10;
+import org.lwjgl.opencl.CLCapabilities;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.ByteBuffer;
@@ -15,10 +18,10 @@ import java.nio.charset.StandardCharsets;
 
 public class ComputeSetup {
     public static void initOpenCL(Logger LOGGER) {
-        Compute.LOGGER = LOGGER;
         LOGGER.info("Initializing OpenCL");
         try (MemoryStack stack = MemoryStack.stackPush()) {
             CL.create();
+            LOGGER.info("Selecting OpenCL Platform");
             IntBuffer numPlatforms = stack.mallocInt(1);
             PointerBuffer platforms;
             CL10.clGetPlatformIDs(null, numPlatforms);
@@ -36,15 +39,41 @@ public class ComputeSetup {
                 ));
             }
             Platform platform = platformSort.first();
+            CLCapabilities platformCapabilities = CL.createPlatformCapabilities(platform.pointer);
             LOGGER.info("Selected OpenCL platform: {}", platform.name);
-            Compute.PLATFORM_CAPABILITIES = CL.createPlatformCapabilities(platform.pointer);
-            PointerBuffer device = stack.mallocPointer(1);
-            CL10.clGetDeviceIDs(platform.pointer, CL10.CL_DEVICE_TYPE_DEFAULT, device, new int[]{1});
-            Compute.DEVICE_CAPABILITIES = CL.createDeviceCapabilities(device.get(0), Compute.PLATFORM_CAPABILITIES);
-            //attributeSize.free();
-            //platforms.free();
-            Runtime.getRuntime().addShutdownHook(new Thread(CL::destroy));
+            LOGGER.info("Selecting OpenCL Devices");
+            PointerBuffer devices;
+            if (platform.cuda) {
+                IntBuffer deviceCount = stack.mallocInt(1);
+                CL10.clGetDeviceIDs(platform.pointer, CL10.CL_DEVICE_TYPE_GPU, null, deviceCount);
+                devices = stack.mallocPointer(deviceCount.get(0));
+                CL10.clGetDeviceIDs(platform.pointer, CL10.CL_DEVICE_TYPE_GPU, devices, deviceCount);
+            } else {
+                devices = stack.mallocPointer(1);
+                CL10.clGetDeviceIDs(platform.pointer, CL10.CL_DEVICE_TYPE_DEFAULT, devices, new int[]{1});
+            }
+            LOGGER.info("Creating OpenCL Context");
+            IntBuffer erret = stack.mallocInt(1);
+            PointerBuffer properties = stack.mallocPointer(3);
+            properties.put(CL10.CL_CONTEXT_PLATFORM);
+            properties.put(platform.pointer);
+            properties.put(0);
+            properties.rewind();
+            final long ctx = CL10.clCreateContext(properties, devices, null, 0, erret);
+            if (ctx < 0) {
+                switch(erret.get(0)) {
+                    case CL10.CL_INVALID_PLATFORM -> throw new InvalidPlatformError(String.format("Platform %s is invalid.", platform.name));
+                    case CL10.CL_DEVICE_NOT_AVAILABLE -> throw new UnavaliableDeviceError("Device is not available.");
+                    case CL10.CL_OUT_OF_RESOURCES, CL10.CL_OUT_OF_HOST_MEMORY -> throw new OutOfMemoryError("Not enough resources available to create OpenCL context.");
+                }
+            }
+            Compute.init(LOGGER, platformCapabilities, CL.createDeviceCapabilities(devices.get(0), platformCapabilities), ctx);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                CL10.clReleaseContext(ctx);
+                CL.destroy();
+            }));
         }
+        LOGGER.info("OpenCL Setup completed successfully");
     }
 
     private static @NonNull String getPlatformAttribute(int attribute, long platform, @NonNull MemoryStack stack) {
