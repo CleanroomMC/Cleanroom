e7ece87a312685bdb4cba6c1ae3151416c9c7bd4
Kuba663 <kuba307@poczta.onet.pl>
2026-07-09 18:43:38 +0200

    Some errors

---
+++ src/main/java/com/cleanroommc/cleanroom/compute/errors/UnavaliableDeviceError.java
@@ -0,0 +1,7 @@
+package com.cleanroommc.cleanroom.compute.errors;
+
+public class UnavaliableDeviceError extends Error {
+    public UnavaliableDeviceError(String message) {
+        super(message);
+    }
+}
