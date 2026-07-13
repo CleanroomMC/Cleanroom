6cc67c7c7e5539bbf8b4b5d9332b0ee364913399
Kuba663 <kuba307@poczta.onet.pl>
2026-07-13 15:05:38 +0200

    Crash on second singleton construction attempt.

--- src/main/java/com/cleanroommc/cleanroom/compute/Compute.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/Compute.java
@@ -1,11 +1,12 @@
 package com.cleanroommc.cleanroom.compute;
 
+import net.lenni0451.reflect.exceptions.ConstructorInvocationException;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.opencl.CLCapabilities;
 
 public class Compute {
 
-    private static Compute INSTANCE;
+    private static Compute INSTANCE = null;
 
     public final CLCapabilities PLATFORM_CAPABILITIES,
             DEVICE_CAPABILITIES;
@@ -26,6 +27,9 @@ public class Compute {
     }
 
     static void init(Logger log, CLCapabilities platform, CLCapabilities device, long context) {
+        if (INSTANCE != null) {
+            throw new ConstructorInvocationException("Second attempt at invoking singleton constructor. ");
+        }
         INSTANCE = new Compute(log, platform, device, context);
     }
 }
