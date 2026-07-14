fa4e441b803128f23876198911988a4f049489e9
Kuba663 <kuba307@poczta.onet.pl>
2026-07-14 15:52:16 +0200

    Invoking kernels from CommandQueue

--- src/main/java/com/cleanroommc/cleanroom/compute/cmd/CommandQueue.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/cmd/CommandQueue.java
@@ -1,16 +1,24 @@
 package com.cleanroommc.cleanroom.compute.cmd;
 
 import com.cleanroommc.cleanroom.compute.Compute;
+import com.cleanroommc.cleanroom.compute.errors.KernelError;
 import com.cleanroommc.cleanroom.compute.errors.UnavaliableDeviceError;
+import com.cleanroommc.cleanroom.compute.kernels.Kernel;
+import com.google.common.base.Preconditions;
+import org.jspecify.annotations.Nullable;
+import org.lwjgl.PointerBuffer;
 import org.lwjgl.opencl.CL10;
 import org.lwjgl.opencl.CL20;
+import org.lwjgl.system.MemoryStack;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Map;
 
 public class CommandQueue implements Closeable {
 
     public final long commandQueue;
+    private final long device;
 
     public CommandQueue(long device) {
         int[] err = new int[1];
@@ -20,6 +28,7 @@ public class CommandQueue implements Closeable {
                 null,
                 err
         );
+        this.device = device;
         switch (err[0]) {
             case CL10.CL_INVALID_CONTEXT -> throw new RuntimeException("Invalid OpenCL context.");
             case CL10.CL_INVALID_DEVICE -> throw new UnavaliableDeviceError("Out of context device provided to command queue.");
@@ -29,8 +38,44 @@ public class CommandQueue implements Closeable {
         }
     }
 
+    public Event dispatchKernel(Kernel kernel,
+                                final PointerBuffer[] arguments,
+                                final long @Nullable [] workGroupOffsets,
+                                final long @Nullable [] workGroupSizes,
+                                final long... dependencies) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            return this.dispatchKernel(stack, kernel, arguments, workGroupOffsets, workGroupSizes, dependencies);
+        }
+    }
+
+    public Event dispatchKernel(MemoryStack stack, Kernel kernel,
+                         final PointerBuffer[] arguments,
+                         final long @Nullable [] workGroupOffsets,
+                         final long @Nullable [] workGroupSizes,
+                         final long... dependencies) {
+        return new Event(kernel.invoke(stack, commandQueue, device, arguments, workGroupOffsets, workGroupSizes, dependencies), stack);
+    }
+
     @Override
     public void close() throws IOException {
         CL20.clReleaseCommandQueue(commandQueue);
+    }
+
+    public final class Event {
+        public final long eventID;
+        private final MemoryStack stack;
+
+        Event(long eventID, MemoryStack stack) {
+            this.eventID = eventID;
+            this.stack = stack;
+        }
+
+        public Event next(Kernel kernel,
+                          final PointerBuffer[] arguments,
+                          final long @Nullable [] workGroupOffsets,
+                          final long @Nullable [] workGroupSizes,
+                          final long... dependencies) {
+            return dispatchKernel(stack, kernel, arguments, workGroupOffsets, workGroupSizes, eventID);
+        }
     }
 }
