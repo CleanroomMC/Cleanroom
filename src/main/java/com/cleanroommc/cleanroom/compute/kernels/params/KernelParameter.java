96fc6ecd0e392ef51001c0bf11eb10e890205166
Kuba663 <kuba307@poczta.onet.pl>
2026-07-18 15:58:41 +0200

    Short Buffer param type

--- src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/KernelParameter.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/KernelParameter.java
@@ -3,6 +3,6 @@ package com.cleanroommc.cleanroom.compute.kernels.params;
 import com.cleanroommc.cleanroom.compute.errors.KernelError;
 
 public sealed interface KernelParameter
-        permits ArrayDoubleParameter, ArrayFloatParameter, ArrayIntParameter, ArrayLongParameter, ArrayShortParameter, BufferByteParameter, BufferDoubleParameter, BufferFloatParameter, ScalarByteParameter, ScalarDoubleParameter, ScalarFloatParameter, ScalarIntegerParameter, ScalarLongParameter, ScalarShortParameter, Vector2bParameter, Vector2dParameter, Vector2fParameter, Vector2iParameter, Vector2lParameter, Vector2sParameter, Vector4bParameter, Vector4dParameter, Vector4fParameter, Vector4iParameter, Vector4lParameter, Vector4sParameter {
+        permits ArrayDoubleParameter, ArrayFloatParameter, ArrayIntParameter, ArrayLongParameter, ArrayShortParameter, BufferByteParameter, BufferDoubleParameter, BufferFloatParameter, BufferShortParameter, ScalarByteParameter, ScalarDoubleParameter, ScalarFloatParameter, ScalarIntegerParameter, ScalarLongParameter, ScalarShortParameter, Vector2bParameter, Vector2dParameter, Vector2fParameter, Vector2iParameter, Vector2lParameter, Vector2sParameter, Vector4bParameter, Vector4dParameter, Vector4fParameter, Vector4iParameter, Vector4lParameter, Vector4sParameter {
     void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError;
 }
