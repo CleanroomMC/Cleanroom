0614f007547de7f61d5126c776079cc9d84b7d8e
Kuba663 <kuba307@poczta.onet.pl>
2026-07-18 11:22:33 +0200

    Vector4f param type

---
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/Vector4fParameter.java
@@ -0,0 +1,14 @@
+package com.cleanroommc.cleanroom.compute.kernels.params;
+
+import com.cleanroommc.cleanroom.compute.errors.KernelError;
+import org.joml.Vector4f;
+import org.lwjgl.opencl.CL10;
+
+import static com.cleanroommc.cleanroom.compute.utils.ErrorUtils.handleKernelParamError;
+
+public record Vector4fParameter(float x, float y, float z, float w) implements KernelParameter {
+    @Override
+    public void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError {
+        handleKernelParamError(CL10.clSetKernelArg4f(kernel, index, x, y, z, w), index, new Vector4f(x,y,z,w));
+    }
+}
