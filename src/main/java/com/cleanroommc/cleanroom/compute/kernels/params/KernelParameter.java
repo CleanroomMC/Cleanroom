adcc202e6f00f476431f9168f2f0668135c1c5ef
Kuba663 <kuba307@poczta.onet.pl>
2026-07-18 12:01:23 +0200

    4 short Vector param type

--- src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/KernelParameter.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/KernelParameter.java
@@ -3,6 +3,6 @@ package com.cleanroommc.cleanroom.compute.kernels.params;
 import com.cleanroommc.cleanroom.compute.errors.KernelError;
 
 public sealed interface KernelParameter
-        permits ScalarByteParameter, ScalarDoubleParameter, ScalarFloatParameter, ScalarIntegerParameter, ScalarLongParameter, ScalarShortParameter, Vector2bParameter, Vector2dParameter, Vector2fParameter, Vector2sParameter, Vector4bParameter, Vector4dParameter, Vector4fParameter {
+        permits ScalarByteParameter, ScalarDoubleParameter, ScalarFloatParameter, ScalarIntegerParameter, ScalarLongParameter, ScalarShortParameter, Vector2bParameter, Vector2dParameter, Vector2fParameter, Vector2sParameter, Vector4bParameter, Vector4dParameter, Vector4fParameter, Vector4sParameter {
     void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError;
 }
