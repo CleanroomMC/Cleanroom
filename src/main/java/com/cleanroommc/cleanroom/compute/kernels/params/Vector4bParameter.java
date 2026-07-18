cf18e0b5b99e8fa4f9af166946c38c077e89cdd9
Kuba663 <kuba307@poczta.onet.pl>
2026-07-18 11:48:07 +0200

    4 byte Vector param type

---
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/Vector4bParameter.java
@@ -0,0 +1,14 @@
+package com.cleanroommc.cleanroom.compute.kernels.params;
+
+import com.cleanroommc.cleanroom.compute.errors.KernelError;
+import org.joml.Vector4i;
+import org.lwjgl.opencl.CL10;
+
+import static com.cleanroommc.cleanroom.compute.utils.ErrorUtils.handleKernelParamError;
+
+public record Vector4bParameter(byte x, byte y, byte z, byte w) implements KernelParameter {
+    @Override
+    public void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError {
+        handleKernelParamError(CL10.clSetKernelArg4b(kernel, index, x, y, z, w), index, new Vector4i(x,y,z,w));
+    }
+}
