d181013369618f7a8dff78b59d1e6e1f69a0ca50
Kuba663 <kuba307@poczta.onet.pl>
2026-07-18 12:31:34 +0200

    2 long Vector param type

--- src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/KernelParameter.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/KernelParameter.java
@@ -3,6 +3,6 @@ package com.cleanroommc.cleanroom.compute.kernels.params;
 import com.cleanroommc.cleanroom.compute.errors.KernelError;
 
 public sealed interface KernelParameter
-        permits ScalarByteParameter, ScalarDoubleParameter, ScalarFloatParameter, ScalarIntegerParameter, ScalarLongParameter, ScalarShortParameter, Vector2bParameter, Vector2dParameter, Vector2fParameter, Vector2iParameter, Vector2sParameter, Vector4bParameter, Vector4dParameter, Vector4fParameter, Vector4iParameter, Vector4sParameter {
+        permits ScalarByteParameter, ScalarDoubleParameter, ScalarFloatParameter, ScalarIntegerParameter, ScalarLongParameter, ScalarShortParameter, Vector2bParameter, Vector2dParameter, Vector2fParameter, Vector2iParameter, Vector2lParameter, Vector2sParameter, Vector4bParameter, Vector4dParameter, Vector4fParameter, Vector4iParameter, Vector4sParameter {
     void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError;
 }
---
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/Vector2lParameter.java
@@ -0,0 +1,13 @@
+package com.cleanroommc.cleanroom.compute.kernels.params;
+
+import com.cleanroommc.cleanroom.compute.errors.KernelError;
+import org.lwjgl.opencl.CL10;
+
+import static com.cleanroommc.cleanroom.compute.utils.ErrorUtils.handleKernelParamError;
+
+public record Vector2lParameter(long x, long y) implements KernelParameter {
+    @Override
+    public void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError {
+        handleKernelParamError(CL10.clSetKernelArg2l(kernel, index, x, y), index, String.format("{%d; %d}", x, y));
+    }
+}
