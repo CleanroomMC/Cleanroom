57ded78b9067913c8cdcb91d890cab521c10b070
Rongmario <rongmario@proton.me>
2026-07-24 14:23:40 +0100

    chore(cleanmix): update mixinbooter's version

--- src/main/java/zone/rong/mixinbooter/Tags.java
+++ src/main/java/zone/rong/mixinbooter/Tags.java
@@ -4,7 +4,7 @@ public final class Tags {
 
     public static final String MOD_ID = "mixinbooter";
     public static final String MOD_NAME = "MixinBooter";
-    public static final String VERSION = "11.2";
+    public static final String VERSION = "11.8";
 
     private Tags() { }
 
