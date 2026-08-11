f1a7764cfd4c717ab7db527c540f81e544331f83
Kuba663 <kuba307@poczta.onet.pl>
2026-08-11 19:51:53 +0200

    ArrayShortParameter preconditions for buffer size
    Ensure that only the 1, 2, 3, 4, 8 and 16 value vectors are allowed.

--- src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/ArrayShortParameter.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/ArrayShortParameter.java
@@ -1,11 +1,19 @@
 package com.cleanroommc.cleanroom.compute.kernels.params;
 
 import com.cleanroommc.cleanroom.compute.errors.KernelError;
+import com.google.common.base.Preconditions;
 import org.lwjgl.opencl.CL10;
 
 import static com.cleanroommc.cleanroom.compute.utils.ErrorUtils.handleKernelParamError;
 
 record ArrayShortParameter(short... values) implements KernelParameter {
+
+    public ArrayShortParameter {
+        Preconditions.checkArgument((values.length > 0 && values.length < 5) || values.length == 8 || values.length == 16,
+                "The only types supported this OpenCL function are: short, short2, short3, short4, short8, short16 and their unsigned variants.");
+    }
+
+
     @Override
     public void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError {
         handleKernelParamError(CL10.clSetKernelArg(kernel, index, values), index, values);
