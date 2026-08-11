9ab13e3f95cb5224d3587a393bac4b0560a26291
Kuba663 <kuba307@poczta.onet.pl>
2026-08-11 19:51:34 +0200

    ArrayIntParameter preconditions for buffer size
    Ensure that only the 1, 2, 3, 4, 8 and 16 value vectors are allowed.

--- src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/ArrayIntParameter.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/ArrayIntParameter.java
@@ -1,11 +1,18 @@
 package com.cleanroommc.cleanroom.compute.kernels.params;
 
 import com.cleanroommc.cleanroom.compute.errors.KernelError;
+import com.google.common.base.Preconditions;
 import org.lwjgl.opencl.CL10;
 
 import static com.cleanroommc.cleanroom.compute.utils.ErrorUtils.handleKernelParamError;
 
 record ArrayIntParameter(int... values) implements KernelParameter {
+
+    public ArrayIntParameter {
+        Preconditions.checkArgument((values.length > 0 && values.length < 5) || values.length == 8 || values.length == 16,
+                "The only types supported this OpenCL function are: int, int2, int3, int4, int8, int16 and their unsigned variants.");
+    }
+
     @Override
     public void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError {
         handleKernelParamError(CL10.clSetKernelArg(kernel, index, values), index, values);
