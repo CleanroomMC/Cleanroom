0ebdd2b4700d8734b95a622e99ea5cff0e6ff4e8
Kuba663 <kuba307@poczta.onet.pl>
2026-08-11 19:51:23 +0200

    ArrayFloatParameter preconditions for buffer size
    Ensure that only the 1, 2, 3, 4, 8 and 16 value vectors are allowed.

--- src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/ArrayFloatParameter.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/kernels/params/ArrayFloatParameter.java
@@ -1,11 +1,18 @@
 package com.cleanroommc.cleanroom.compute.kernels.params;
 
 import com.cleanroommc.cleanroom.compute.errors.KernelError;
+import com.google.common.base.Preconditions;
 import org.lwjgl.opencl.CL10;
 
 import static com.cleanroommc.cleanroom.compute.utils.ErrorUtils.handleKernelParamError;
 
 record ArrayFloatParameter(float... values) implements KernelParameter {
+
+    public ArrayFloatParameter {
+        Preconditions.checkArgument((values.length > 0 && values.length < 5) || values.length == 8 || values.length == 16,
+                "The only types supported this OpenCL function are: float, float2, float3, float4, float8 and float16.");
+    }
+
     @Override
     public void bindParameter(long kernel, int index) throws KernelError, OutOfMemoryError {
         handleKernelParamError(CL10.clSetKernelArg(kernel, index, values), index, values);
