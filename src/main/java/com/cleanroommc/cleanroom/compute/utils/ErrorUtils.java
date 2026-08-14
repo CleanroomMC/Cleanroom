a3423210f292cfb1cca65645407a13687072b234
Kuba663 <kuba307@poczta.onet.pl>
2026-08-14 19:01:16 +0200

    Function for handling copy image errors

--- src/main/java/com/cleanroommc/cleanroom/compute/utils/ErrorUtils.java
+++ src/main/java/com/cleanroommc/cleanroom/compute/utils/ErrorUtils.java
@@ -31,4 +31,17 @@ public record ErrorUtils() {
             case CL10.CL_OUT_OF_RESOURCES, CL10.CL_OUT_OF_HOST_MEMORY -> throw new OutOfMemoryError("Not enough resources to fill image.");
         }
     }
+    public static void handleEnqueueCopyImageError(int err) {
+        switch (err) {
+            case CL10.CL_INVALID_MEM_OBJECT -> throw new ImageError("Image is not a valid memory object.");
+            case CL10GL.CL_INVALID_MIP_LEVEL -> throw new ImageError("Provided mipmap level is invalid.");
+            case CL10.CL_INVALID_EVENT_WAIT_LIST -> throw new ImageError("One or more events provided as dependencies is invalid.");
+            case CL12.CL_INVALID_IMAGE_SIZE -> throw new ImageError("Image size exceeds the maximum size of the device.");
+            case CL12.CL_IMAGE_FORMAT_NOT_SUPPORTED -> throw new ImageError("Image format unsupported by device.");
+            case CL10.CL_MEM_OBJECT_ALLOCATION_FAILURE -> throw new ImageError("Failed to allocate memory for image fill operation");
+            case CL10.CL_INVALID_OPERATION -> throw new ImageError("Device associated with command queue does not support images.");
+            case CL10.CL_OUT_OF_RESOURCES, CL10.CL_OUT_OF_HOST_MEMORY -> throw new OutOfMemoryError("Not enough resources to copy image.");
+            case CL12.CL_MEM_COPY_OVERLAP -> throw new ImageError("Copy regions overlapping.");
+        }
+    }
 }
