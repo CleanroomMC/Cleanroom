c0b13c3eb00d7883993bcbc97d3c4eda367c6350
Kuba663 <kuba307@poczta.onet.pl>
2026-08-23 19:44:28 +0200

    Image1DBuffer implementation

--- module/cleanroom/src/main/java/com/cleanroommc/compute/images/Image1DBuffer.java
+++ module/cleanroom/src/main/java/com/cleanroommc/compute/images/Image1DBuffer.java
@@ -2,6 +2,7 @@ package com.cleanroommc.compute.images;
 
 import com.cleanroommc.compute.buffers.Buffer;
 import com.cleanroommc.compute.buffers.BufferFlags;
+import com.cleanroommc.compute.cmd.CommandQueue;
 import com.cleanroommc.compute.errors.ImageError;
 import com.cleanroommc.kirino.gl.texture.GLTexture;
 import com.google.common.base.Preconditions;
@@ -39,84 +40,84 @@ public final class Image1DBuffer extends Image1D {
     }
 
     @Override
-    public <B extends java.nio.Buffer> long fill(@NonNull MemoryStack stack, long commandQueue, @NonNull B color, @NonNull Long from, @NonNull Long size, int mipmap, long... dependencies) {
+    public <B extends java.nio.Buffer> long fill(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull B color, @NonNull Long from, @NonNull Long size, int mipmap, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.fill(stack, commandQueue, color, from, size, mipmap, dependencies);
     }
 
     @Override
-    public long fill(@NonNull MemoryStack stack, long commandQueue, int @NonNull [] color, @NonNull Long from, @NonNull Long size, int mipmap, long... dependencies) {
+    public long fill(@NonNull MemoryStack stack, CommandQueue commandQueue, int @NonNull [] color, @NonNull Long from, @NonNull Long size, int mipmap, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.fill(stack, commandQueue, color, from, size, mipmap, dependencies);
     }
 
     @Override
-    public long fill(@NonNull MemoryStack stack, long commandQueue, float @NonNull [] color, @NonNull Long from, @NonNull Long size, int mipmap, long... dependencies) {
+    public long fill(@NonNull MemoryStack stack, CommandQueue commandQueue, float @NonNull [] color, @NonNull Long from, @NonNull Long size, int mipmap, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.fill(stack, commandQueue, color, from, size, mipmap, dependencies);
     }
 
     @Override
-    public <CT2> long copy(@NonNull MemoryStack stack, long commandQueue, @NonNull Image<CT2> destination, @NonNull Long from, int fromMipmap, @NonNull CT2 to, int toMipmap, @NonNull CT2 size, long... dependencies) {
+    public <CT2> long copy(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Image<CT2> destination, @NonNull Long from, int fromMipmap, @NonNull CT2 to, int toMipmap, @NonNull CT2 size, long... dependencies) {
         return super.copy(stack, commandQueue, destination, from, fromMipmap, to, toMipmap, size, dependencies);
     }
 
     @Override
-    public <B extends java.nio.Buffer> long read(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, @NonNull B buffer, boolean blocking, long... dependencies) {
+    public <B extends java.nio.Buffer> long read(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, @NonNull B buffer, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.read(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, buffer, blocking, dependencies);
     }
 
     @Override
-    public long read(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, short @NonNull [] array, boolean blocking, long... dependencies) {
+    public long read(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, short @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.read(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public long read(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, int @NonNull [] array, boolean blocking, long... dependencies) {
+    public long read(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, int @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.read(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public long read(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, float @NonNull [] array, boolean blocking, long... dependencies) {
+    public long read(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, float @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.read(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public long read(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, double @NonNull [] array, boolean blocking, long... dependencies) {
+    public long read(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, double @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.read(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public <B extends java.nio.Buffer> long write(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, @NonNull B buffer, boolean blocking, long... dependencies) {
+    public <B extends java.nio.Buffer> long write(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, @NonNull B buffer, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.write(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, buffer, blocking, dependencies);
     }
 
     @Override
-    public long write(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, short @NonNull [] array, boolean blocking, long... dependencies) {
+    public long write(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, short @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.write(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public long write(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, int @NonNull [] array, boolean blocking, long... dependencies) {
+    public long write(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, int @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.write(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public long write(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, float @NonNull [] array, boolean blocking, long... dependencies) {
+    public long write(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, float @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.write(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
 
     @Override
-    public long write(@NonNull MemoryStack stack, long commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, double @NonNull [] array, boolean blocking, long... dependencies) {
+    public long write(@NonNull MemoryStack stack, CommandQueue commandQueue, @NonNull Long from, int mipmap, @NonNull Long size, long rowPitch, long slicePitch, double @NonNull [] array, boolean blocking, long... dependencies) {
         Preconditions.checkArgument(mipmap == 0);
         return super.write(stack, commandQueue, from, mipmap, size, rowPitch, slicePitch, array, blocking, dependencies);
     }
