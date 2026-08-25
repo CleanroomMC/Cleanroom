fc10e98dbc8af8b93d11c993bf91c6c98dc7f9a1
Kuba663 <kuba307@poczta.onet.pl>
2026-08-25 19:46:12 +0200

    Document CommandQueueDispatch

--- module/cleanroom/src/main/java/com/cleanroommc/compute/cmd/CommandQueueDispatch.java
+++ module/cleanroom/src/main/java/com/cleanroommc/compute/cmd/CommandQueueDispatch.java
@@ -8,20 +8,39 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectAVLTreeMap;
 import org.jspecify.annotations.NonNull;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Allows for the selection of CommandQueues based on device capabilities.
+ * @author EΣrie
+ */
 public final class CommandQueueDispatch implements Closeable {
 
     private int currDevice = 0;
     private final Map<String, CommandQueue> commandQueues = new Object2ObjectAVLTreeMap<>();
 
+    /**
+     * Select any device and return a CommandQueue.
+     * @param name name of the CommandQueue
+     * @return the CommandQueue
+     * @author EΣrie
+     */
     public CommandQueue dispatch(@NonNull String name) {
         return dispatch(name, false, false, false);
     }
 
-    public CommandQueue dispatch(@NonNull String name, boolean images, boolean mipmaps, boolean pipes) {
+    /**
+     * Select a device based on the given capabilities and return a CommandQueue.
+     * @param name name of the CommandQueue
+     * @param images should the device support images?
+     * @param mipmaps should the device support mipmapped images?
+     * @param pipes should the device support pipes?
+     * @return the CommandQueue
+     * @throws UnavaliableDeviceError if no device is found matching the given capabilities.
+     * @author EΣrie
+     */
+    public CommandQueue dispatch(@NonNull String name, boolean images, boolean mipmaps, boolean pipes) throws UnavaliableDeviceError {
         Preconditions.checkNotNull(name);
         int startDevice = currDevice;
         do {
@@ -38,13 +57,23 @@ public final class CommandQueueDispatch implements Closeable {
         throw new UnavaliableDeviceError("No device found matching requirements.");
     }
 
+    /**
+     * Get CommandQueue by name.
+     * @param name name of the CommandQueue
+     * @return the CommandQueue, or empty if it doesn't exist.
+     * @author EΣrie
+     */
     public @NonNull Optional<CommandQueue> get(@NonNull String name) {
         Preconditions.checkNotNull(name);
         return Optional.ofNullable(commandQueues.get(name));
     }
 
+    /**
+     * Close all CommandQueues.
+     * @author EΣrie
+     */
     @Override
-    public void close() throws IOException {
+    public void close() {
         for (CommandQueue queue : commandQueues.values()) {
             queue.close();
         }
