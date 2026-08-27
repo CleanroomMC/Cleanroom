eef6bf9fc8a45891cbb67683ad4519b1435cca33
Kuba663 <kuba307@poczta.onet.pl>
2026-08-27 14:22:45 +0200

    Document Device

--- module/cleanroom/src/main/java/com/cleanroommc/compute/Device.java
+++ module/cleanroom/src/main/java/com/cleanroommc/compute/Device.java
@@ -3,6 +3,16 @@ package com.cleanroommc.compute;
 import org.jspecify.annotations.NonNull;
 import org.lwjgl.opencl.CLCapabilities;
 
+/**
+ * [<i>Internal Class</i>] OpenCL Device
+ * @param handle the handle to the device
+ * @param capabilities the capabilities of the device
+ * @param maxWorkItemSizes the maximum work item sizes supported by the device
+ * @param supportsImages whether the device supports images
+ * @param supportsMipmaps whether the device supports mipmaps
+ * @param supportsPipes whether the device supports pipes
+ * @author EΣrie
+ */
 public record Device(long handle,
                      CLCapabilities capabilities,
                      long[] maxWorkItemSizes,
