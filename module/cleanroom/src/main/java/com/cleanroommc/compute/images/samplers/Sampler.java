8c0241798767ebaea02c87739fdda60cc4d47c03
Kuba663 <kuba307@poczta.onet.pl>
2026-08-27 10:41:58 +0200

    Document Sampler

--- module/cleanroom/src/main/java/com/cleanroommc/compute/images/samplers/Sampler.java
+++ module/cleanroom/src/main/java/com/cleanroommc/compute/images/samplers/Sampler.java
@@ -3,6 +3,7 @@ package com.cleanroommc.compute.images.samplers;
 import com.cleanroommc.compute.Compute;
 import com.cleanroommc.compute.errors.ImageError;
 import com.cleanroommc.compute.smrtptr.SmartPointer;
+import com.google.common.base.Preconditions;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.lwjgl.opencl.CL10;
@@ -13,22 +14,67 @@ import org.lwjgl.system.MemoryStack;
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 
+/**
+ * OpenCL sampler.
+ * @apiNote I don't know why you would use that. There might be a reason, but I don't know it.
+ * Unless you really need to use samplers created on the host side, you should create them directly in CL code.
+ * @author EΣrie
+ */
 public final class Sampler extends SmartPointer {
 
+    /**
+     * Is coordinate [0;0] in the centre.
+     */
     public final boolean normalized;
+    /**
+     * {@link AddressingMode} of the sampled image.
+     */
     public final @NonNull AddressingMode addressingMode;
+    /**
+     * {@link FilteringMode} of the sampled image.
+     */
     public final @NonNull FilteringMode filteringMode;
+    /**
+     * {@link FilteringMode} of the mipmaps of the sampled image.
+     * @apiNote If sampling the mipmaps is not necessary, set to null.
+     */
     public final @Nullable FilteringMode mipmapFilteringMode;
+    /**
+     * Minimum LOD.
+     * @apiNote If sampling the mipmaps is not necessary, this stays unused, set it to something like 0 or -1.
+     */
     public final float levelOfDetailMinimum;
+    /**
+     * Maximum LOD.
+     * @apiNote If sampling the mipmaps is not necessary, this stays unused, set it to something like 0 or -1.
+     */
     public final float levelOfDetailMaximum;
+    /**
+     * OpenCL sampler handle.
+     */
     public final long handle;
 
+    /**
+     * <p>Create a sampler.</p>
+     * <p>For more details on the parameters, look to the documentation of the variables.</p>
+     * @param normalized Is coordinate [0;0] in the centre.
+     * @param addressingMode {@link AddressingMode} of the sampled image.
+     * @param filteringMode {@link FilteringMode} of the sampled image.
+     * @param mipmapFilteringMode {@link FilteringMode} of the mipmaps of the sampled image.
+     *                                                 See: {@link #mipmapFilteringMode}.
+     * @param levelOfDetailMinimum Minimum LOD. See: {@link #levelOfDetailMinimum}.
+     * @param levelOfDetailMaximum Maximum LOD. See: {@link #levelOfDetailMaximum}.
+     * @author EΣrie
+     */
     public Sampler(boolean normalized,
                    @NonNull AddressingMode addressingMode,
                    @NonNull FilteringMode filteringMode,
                    @Nullable FilteringMode mipmapFilteringMode,
                    float levelOfDetailMinimum,
                    float levelOfDetailMaximum) {
+        Preconditions.checkNotNull(filteringMode);
+        Preconditions.checkNotNull(addressingMode);
+        Preconditions.checkState(mipmapFilteringMode == null || levelOfDetailMinimum <= levelOfDetailMaximum);
         super();
         this.normalized = normalized;
         this.addressingMode = addressingMode;
@@ -58,6 +104,10 @@ public final class Sampler extends SmartPointer {
         }
     }
 
+    /**
+     * Closes the sampler and releases memory.
+     * @author EΣrie
+     */
     @Override
     public void close() {
         super.close();
