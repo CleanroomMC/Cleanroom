b0a9ec02aea7a94b90b02d6615f6ee4bc6aab9ae
Kuba663 <kuba307@poczta.onet.pl>
2026-09-24 16:36:25 +0200

    ClientSetupTest
    passes

---
+++ module/cleanroom/src/test/java/com/cleanroommc/compute/ClientSetupTest.java
@@ -0,0 +1,24 @@
+package com.cleanroommc.compute;
+
+import com.cleanroommc.test.kirino.gl.ext.GLTestExtension;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.lwjgl.system.Configuration;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@ExtendWith(GLTestExtension.class)
+public class ClientSetupTest {
+    @Test
+    public void setupTestClient() {
+        GLTestExtension.assumeInitialized();
+        GLTestExtension.submit(() -> {
+            GLTestExtension.assumeGL46();
+            Logger testLogger = LogManager.getLogger("TestLogger");
+            assertDoesNotThrow(() -> Configuration.OPENCL_EXPLICIT_INIT.set(true));
+            assertDoesNotThrow(() -> ComputeSetup.initOpenCL(testLogger, true));
+        });
+    }
+}
